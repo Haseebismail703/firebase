6fae7387199897214dc589805336693668336af4
Haseebismail703 <haseebismail703@gmail.com>
2023-12-08 12:11:56 +0500

    kl

--- saylani form/app.js
+++ saylani form/app.js
@@ -1,7 +1,7 @@
 
  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
- import { getAuth , createUserWithEmailAndPassword , signInWithEmailAndPassword   } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
- import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+ import { getAuth , createUserWithEmailAndPassword   } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+ import { getFirestore ,collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
  
 
 
@@ -21,5 +21,88 @@
  const auth = getAuth(app);
  const db = getFirestore(app);
 
+
+
+ let btn = document.getElementById('btn')
+if(btn){
+let karachi = document.getElementById('karachi')
+let Select = document.getElementById('Select')
+let Full = document.getElementById('Full')
+let Father= document.getElementById('Father')
+let email = document.getElementById('email')
+let Phone = document.getElementById('Phone')
+let getCNIC = document.getElementById('CNIC')
+let Address= document.getElementById('Address')
+let Date = document.getElementById('Date')
+let gender = document.getElementById('gender')
+let qualification = document.getElementById('qualification')
+let Laptop = document.getElementById('Laptop')
+
+btn.addEventListener('click',()=>{
+
+
+    createUserWithEmailAndPassword(auth, karachi.value, Select.value)
+    .then(async(userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      console.log(user)
+
+  
+
+
+      // Full.value=""
+      // password.value = ''
+      // ...
+
+      
+
+     
+      try {
+        const docRef = await addDoc(collection(db, "users"), {
+        karachi: karachi.value,
+        Full: Full.value ,
+        Select: Select.value ,
+        Father: Father.value,
+        email: email.value ,
+        Phone: Phone.value ,
+        getCNIC: getCNIC.value,
+        Address: Address.value ,
+        Date: Date.value ,
+        gender: gender.value,
+        qualification: qualification.value ,
+        Laptop: Laptop.value ,
+     
+
+
+
+         
+        });
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+      
+   
+
+// location.href="./login.html"
+
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage)
+
+
+    
+
+
+      // ..
+
+
+    });
+
+})
+
+}
 
  
