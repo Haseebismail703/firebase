ec1fef7b2541b22355fef561ae9f983a2ec35db7
Haseebismail703 <haseebismail703@gmail.com>
2023-12-12 20:38:48 +0500

    pic

--- storage/app.js
+++ storage/app.js
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getStorage, ref, uploadBytes } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+import { getStorage, ref, uploadBytes , uploadBytesResumable, getDownloadURL , deleteObject } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD0FjVwlw_dnXudam_YmvtG-yipsLD8efg",
@@ -22,17 +22,101 @@ const storage = getStorage(app);
 
 let upload = document.getElementById('Uplode')
 
-upload.addEventListener('click',()=>{
 
-let file = document.getElementById('file')
+let uploadfile = (file) => {
 
-  const storageRef = ref(storage, `images/${file.text[0].name}`);
+  return new Promise((resolve,reject,)=>{
+  const storageRef = ref(storage, `images/${file.name}`);
 
-// 'file' comes from the Blob or File API
-    uploadBytes(storageRef, file.text[0]).then((snapshot) => {
-    console.log('Uploaded a blob or file!');
+   // 'file' comes from the Blob or File API
+    uploadBytes(storageRef, file).then((snapshot) => {
+    // console.log('Uploaded a blob or file!');
+
+
+
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    
+    uploadTask.on('state_changed', 
+      (snapshot) => {
+        
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        // console.log('Upload is 100% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+        }
+      
+
+
+      }, 
+      (error) => {
+        // Handle unsuccessful uploads
+        reject(error)
+      }, 
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          // console.log('File available at', downloadURL);
+
+          resolve(downloadURL)
+        });
+      }
+    );
 
 
 
   });
+
+
+    
+  })
+
+
+}
+
+
+
+
+upload.addEventListener('click',async()=>{
+
+  try{
+    let file = document.getElementById('file')
+ const res =  await uploadfile(file.files[0])
+ let img = document.getElementById('img')
+ img.src= res
+ console.log('err--->',res);
+   
+  }catch(err){
+   console.log(err);
+
+  }
+
 })
+
+
+let delbtn =document.getElementById('delete')
+
+delbtn.addEventListener('click',()=>{
+
+// Creae a reference to the file to delete
+const desertRef = ref(storage, 'images/wave-haikei.png');
+
+// Delete the file
+deleteObject(desertRef).then((res) => {
+  console.log(res,'File deleted successfully');
+}).catch((error) => {
+  console.log(error);
+});
+
+
+
+
+})
+
+
+
